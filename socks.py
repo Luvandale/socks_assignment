8e309c7ab80680c5c5b9b03de1ae76d1328fba94
Luvandale <55182326+Luvandale@users.noreply.github.com>
2021-11-05 17:52:15 +0300

    first commit

---
+++ socks.py
@@ -0,0 +1,39 @@
+from collections import Counter
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+  socks_count = Counter(ar)
+  count_values = socks_count.values()
+  result = 0
+  for i in count_values:
+    val = i//2
+    if(val>0):
+      result += val
+  return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
